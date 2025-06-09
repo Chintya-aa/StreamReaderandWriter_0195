f6bf11b825fdd18ff14c9754e2a42eaf8d8b0221
Chintya-aa <tyachintyachin@gmail.com>
2025-06-09 14:29:51 +0700

    membuat objek output

---
+++ iofile.cpp
@@ -0,0 +1,13 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string baris;
+
+    //membuat objek output file stream.
+    ofstream outfile;
+    
+}
+
